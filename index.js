63e67ffa88e454fe7ecfd1ff91e02fed9f5f1d54
David Lefrancq <david.lefrancq@gmail.com>
2021-07-02 12:28:25 +0200

    Add commentary

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const {Gaufrier} = require('./bo/Gaufier.js')
 
+// Tableau de Gaufier
 const listGaufier = [];
 
+// Fonction d'initialisation
 function init() {
     const obj1 = new Gaufrier('seb',100,50,600);
     listGaufier.push(obj1);
@@ -10,15 +12,21 @@ function init() {
     listGaufier.push(obj2);
 }
 
+// Exécution de l'init
 init();
 
+// Affichage
 console.log(listGaufier);
 console.log(listGaufier[0].getModel());
+
+// Modification
 listGaufier[0].setModel("jack");
 console.log(listGaufier[0].getModel());
 
+// Affichage du tableau modifier
 console.log(listGaufier);
 
+// Boucle de lecture / foreach
 // for(const key in listGaufier){
 //     gaufier = listGaufier[key];
 //     console.log(gaufier.getModel());
